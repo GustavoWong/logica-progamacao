c131cefc2c18c4328e7db4e2ff217bf91f757ca1
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-05-28 00:06:27 -0300

    feat: updates

--- ex.43/index.js
+++ ex.43/index.js
@@ -2,13 +2,6 @@
 Escreva um programa que imprima todos os números pares do intervalo fechado de 1 a 100.
 */
 
-for (let i = 1; i < 100; i++) {
-  let isPar = i % 2 === 0 ? false : isPar;
-  if(isPar){
-      console.log(isPar);
-    return 'impar'
-  } else {
-    console.log(isPar);
-    return 'isPar[i]'
-  }
+for (let i = 2; i <= 100; i += 2) {
+  console.log(i);
 }
